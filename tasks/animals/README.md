6992a4ceb18874b878ffb2f0fbf0e389c841d604
sebastiancallegl <sebastian.calle@globant.com>
2025-12-11 17:50:35 -0500

    adding new day

---
+++ tasks/animals/README.md
@@ -0,0 +1,13 @@
+1. create a function that returns the number of animals that have a specific color, the function should be named "getAnimalsByColor", the function should receive only one parameter (the color the animal should have)
+
+if the animal's color is: "White, Brown" and the color that we are searching is "Brown" the animal meets the color requirement.
+
+if the animal's color is "Red-brown" and the color that we are searching is "red" the animal meets the color requirement.
+
+notes:
+
+- there are 8 animals with the color "Brown"
+
+- there is only 1 animal with the color "Blue"
+
+- there are 2 animals with the color "Red"
---
+++ tasks/animals/animals.js
@@ -0,0 +1,202 @@
+const animalsCollection = [
+    // 1. Lion
+    {
+        species: "Panthera leo",
+        name: "Lion",
+        isCarnivore: true,
+        age: 7,
+        weightKg: 190.5,
+        color: "Golden",
+        habitat: ["Savanna", "Grasslands"],
+    },
+    // 2. Giraffe
+    {
+        species: "Giraffa camelopardalis",
+        name: "Giraffe",
+        isCarnivore: false,
+        age: 4,
+        weightKg: 1100,
+        color: "Spotted Brown",
+        habitat: ["Savanna", "Open Woodlands"],
+    },
+    // 3. Cheetah
+    {
+        species: "Acinonyx jubatus",
+        name: "Cheetah",
+        isCarnivore: true,
+        age: 3,
+        weightKg: 55,
+        color: "Yellow/Black Spots",
+        habitat: ["Savanna", "Semi-desert"],
+    },
+    // 4. African Elephant
+    {
+        species: "Loxodonta africana",
+        name: "African Elephant",
+        isCarnivore: false,
+        age: 15,
+        weightKg: 6000,
+        color: "Grey",
+        habitat: ["Savanna", "Forests"],
+    },
+    // 5. Brown Bear
+    {
+        species: "Ursus arctos",
+        name: "Brown Bear",
+        isCarnivore: true,
+        age: 10,
+        weightKg: 350,
+        color: "Brown",
+        habitat: ["Forests", "Mountains"],
+    },
+    // 6. Gray Wolf
+    {
+        species: "Canis lupus",
+        name: "Gray Wolf",
+        isCarnivore: true,
+        age: 5,
+        weightKg: 45,
+        color: "Gray",
+        habitat: ["Forests", "Tundra"],
+    },
+    // 7. Killer Whale
+    {
+        species: "Orcinus orca",
+        name: "Killer Whale",
+        isCarnivore: true,
+        age: 12,
+        weightKg: 5000,
+        color: "Black & White",
+        habitat: ["Ocean"],
+    },
+    // 8. Orangutan
+    {
+        species: "Pongo pygmaeus",
+        name: "Orangutan",
+        isCarnivore: false,
+        age: 8,
+        weightKg: 80,
+        color: "Red-brown",
+        habitat: ["Tropical Rainforest"],
+    },
+    // 9. Komodo Dragon
+    {
+        species: "Varanus komodoensis",
+        name: "Komodo Dragon",
+        isCarnivore: true,
+        age: 6,
+        weightKg: 90,
+        color: "Dark Grey",
+        habitat: ["Tropical Islands"],
+    },
+    // 10. Emperor Penguin
+    {
+        species: "Aptenodytes forsteri",
+        name: "Emperor Penguin",
+        isCarnivore: true,
+        age: 9,
+        weightKg: 30,
+        color: "Black, White, Yellow",
+        habitat: ["Antarctic Ice"],
+    },
+    // 11. Red Kangaroo
+    {
+        species: "Macropus rufus",
+        name: "Red Kangaroo",
+        isCarnivore: false,
+        age: 5,
+        weightKg: 75,
+        color: "Reddish-brown",
+        habitat: ["Outback", "Desert"],
+    },
+    // 12. Atlantic Cod
+    {
+        species: "Gadus morhua",
+        name: "Atlantic Cod",
+        isCarnivore: true,
+        age: 2,
+        weightKg: 12,
+        color: "Green-brown",
+        habitat: ["North Atlantic Ocean"],
+    },
+    // 13. Blue Whale
+    {
+        species: "Balaenoptera musculus",
+        name: "Blue Whale",
+        isCarnivore: false,
+        age: 30,
+        weightKg: 150000,
+        color: "Mottled Blue-Gray",
+        habitat: ["Open Ocean"],
+    },
+    // 14. Ostrich
+    {
+        species: "Struthio camelus",
+        name: "Ostrich",
+        isCarnivore: false,
+        age: 11,
+        weightKg: 140,
+        color: "Black/White",
+        habitat: ["Savanna", "Desert"],
+    },
+    // 15. American Alligator
+    {
+        species: "Alligator mississippiensis",
+        name: "American Alligator",
+        isCarnivore: true,
+        age: 25,
+        weightKg: 450,
+        color: "Olive Green",
+        habitat: ["Swamps", "Marshes"],
+    },
+    // 16. Olive Baboon
+    {
+        species: "Papio anubis",
+        name: "Olive Baboon",
+        isCarnivore: false,
+        age: 8,
+        weightKg: 30,
+        color: "Olive-Green/Brown",
+        habitat: ["Savanna", "Rocky Areas"],
+    },
+    // 17. Asian Elephant
+    {
+        species: "Elephas maximus",
+        name: "Asian Elephant",
+        isCarnivore: false,
+        age: 18,
+        weightKg: 5000,
+        color: "Dark Gray",
+        habitat: ["Tropical Forests"],
+    },
+    // 18. Koala
+    {
+        species: "Phascolarctos cinereus",
+        name: "Koala",
+        isCarnivore: false,
+        age: 6,
+        weightKg: 9,
+        color: "Gray",
+        habitat: ["Eucalyptus Forests"],
+    },
+    // 19. Arctic Fox
+    {
+        species: "Alopex lagopus",
+        name: "Arctic Fox",
+        isCarnivore: true,
+        age: 2,
+        weightKg: 4,
+        color: "White (Winter), Brown (Summer)",
+        habitat: ["Tundra", "Arctic"],
+    },
+    // 20. Horse (Mustang)
+    {
+        species: "Equus caballus",
+        name: "Horse (Mustang)",
+        isCarnivore: false,
+        age: 9,
+        weightKg: 400,
+        color: "Brown, Black, Tan",
+        habitat: ["Prairies", "Steppes"],
+    }
+];
